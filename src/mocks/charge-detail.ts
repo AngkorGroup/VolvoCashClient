8dc48506844cb1e034a8a752e183bb37d7d75638
Juan José Ramírez <jjramirez@pucp.pe>
2020-10-29 07:26:33 -0500

    Add date to charge

--- src/mocks/charge-detail.ts
+++ src/mocks/charge-detail.ts
@@ -4,6 +4,7 @@ export default new Charge({
   id: 1,
   amount: 1200.2,
   currency: 'PEN',
+  date: '1985-06-12T23:20:50.52Z',
   displayName: 'Lubricante HD-5000',
   cashier: {
     firstName: 'Luis',
