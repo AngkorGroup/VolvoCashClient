1c75b43840615ecde3b8f7d1340d363faa3a654a
Juan José Ramírez <jjramirez@pucp.pe>
2020-11-08 22:48:28 -0500

    Lint changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,3 @@
 export const SMS_CODE_LENGTH = 4;
-export const ANDROID_VERSION = "v1.0.1";
-export const IOS_VERSION = "v1.0.1";
-
+export const ANDROID_VERSION = 'v1.0.1';
+export const IOS_VERSION = 'v1.0.1';
