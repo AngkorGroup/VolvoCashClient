447a03dc1fb78ff3354ed7f9de46b17b4ef17dda
Juan José Ramírez <jjramirez@pucp.pe>
2020-12-22 13:33:57 -0500

    Bump version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const SMS_CODE_LENGTH = 4;
-export const ANDROID_VERSION = 'v1.4.0 (14)';
-export const IOS_VERSION = 'v1.4.0 (22)';
+export const ANDROID_VERSION = 'v1.4.1 (15)';
+export const IOS_VERSION = 'v1.4.1 (23)';
